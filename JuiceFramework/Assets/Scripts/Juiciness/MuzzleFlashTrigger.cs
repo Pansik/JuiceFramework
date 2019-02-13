81c88620f75cca26bc291ccf408784acb20bc056
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2019-02-13 22:02:49 +0100

    Randomize muzzle flash, fix muzzle flash position

--- JuiceFramework/Assets/Scripts/Juiciness/MuzzleFlashTrigger.cs
+++ JuiceFramework/Assets/Scripts/Juiciness/MuzzleFlashTrigger.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class MuzzleFlashTrigger : MonoBehaviour
 {
     public GameObject muzzleFlashLight;
+    public float minOffset = 0.05f;
+    public float maxOffset = 0.1f;
+    public float maxIntensity = 6.5f;
+    public float minIntensity = 4.5f;
+    public float maxRange = 2f;
+    public float minRange = 1.2f;
+
+    private Vector3 originalPosition;
 
     void Start()
     {
+        originalPosition = muzzleFlashLight.transform.position;
         Shooting.EventBulletShooted += DoMuzzleFlash;    
     }
 
@@ -18,8 +27,30 @@ public class MuzzleFlashTrigger : MonoBehaviour
 
     private IEnumerator MuzzleFlash()
     {
+        RandomizePosition();
+        RandomizeIntensity();
+        RandomizeRange();
         muzzleFlashLight.SetActive(true);
         yield return new WaitForEndOfFrame();
         muzzleFlashLight.SetActive(false);
+        muzzleFlashLight.transform.position = originalPosition;
+    }
+
+    private void RandomizePosition()
+    {
+        muzzleFlashLight.transform.position = new Vector3(
+            muzzleFlashLight.transform.position.x,
+            muzzleFlashLight.transform.position.y + Random.Range(minOffset, maxOffset),
+            +muzzleFlashLight.transform.position.z + Random.Range(minOffset, maxOffset));
+    }
+
+    private void RandomizeIntensity()
+    {
+        muzzleFlashLight.GetComponent<Light>().intensity = Random.Range(minIntensity, maxIntensity);
+    }
+
+    private void RandomizeRange()
+    {
+        muzzleFlashLight.GetComponent<Light>().range = Random.Range(maxRange, minRange);
     }
 }
