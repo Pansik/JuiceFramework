94406f041287e9ba634ebdae3f7ce7d2323b368e
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2019-02-13 21:51:55 +0100

    Add muzzle flash

---
+++ JuiceFramework/Assets/Scripts/Juiciness/MuzzleFlashTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlashTrigger : MonoBehaviour
+{
+    public GameObject muzzleFlashLight;
+
+    void Start()
+    {
+        Shooting.EventBulletShooted += DoMuzzleFlash;    
+    }
+
+    private void DoMuzzleFlash()
+    {
+        StartCoroutine(MuzzleFlash());
+    }
+
+    private IEnumerator MuzzleFlash()
+    {
+        muzzleFlashLight.SetActive(true);
+        yield return new WaitForEndOfFrame();
+        muzzleFlashLight.SetActive(false);
+    }
+}
