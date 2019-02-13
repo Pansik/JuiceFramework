6e112a16febc81f8df5b58bdcf3f366c5310c766
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2019-02-13 21:03:07 +0100

    Add shoot animation and idle animation

---
+++ JuiceFramework/Assets/Scripts/Juiciness/ShootAnimTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAnimTrigger : MonoBehaviour
+{
+    public string triggerNameForShoot;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        Shooting.EventBulletShooted += TriggerShootAnimation;
+    }
+
+    private void TriggerShootAnimation()
+    {
+        animator.Play(triggerNameForShoot, -1, 0);
+    }
+}
