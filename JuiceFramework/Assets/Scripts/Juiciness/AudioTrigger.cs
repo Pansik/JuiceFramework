2fe105f7f48840732586be9e219a902d4d0ab895
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2019-02-13 21:24:58 +0100

    Add shooting sound with randomization

---
+++ JuiceFramework/Assets/Scripts/Juiciness/AudioTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioTrigger : MonoBehaviour
+{
+    public float maxPitch;
+    public float minPitch;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Shooting.EventBulletShooted += PlaySound;
+    }
+
+    private void PlaySound()
+    {
+        RandomizePitch();
+        audioSource.Play();
+    }
+
+    private void RandomizePitch()
+    {
+        audioSource.pitch = Random.Range(minPitch, maxPitch);
+    }
+}
